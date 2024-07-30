23e210ac77d037cb9426261d2b1731703870c237
Karun <karunpr1@uni-bremen.de>
2024-07-30 10:17:13 +0200

    [MT-001] modified eval script for the russian price labels dataset

--- downstream/object_detection/eval_object_detection.py
+++ downstream/object_detection/eval_object_detection.py
@@ -3,13 +3,15 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2 import model_zoo
 from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.structures import BoxMode
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
 import cv2
-import random
-
 import os
 import json
-from detectron2.data import DatasetCatalog, MetadataCatalog
-from detectron2.structures import BoxMode
+import random
 
 
 def get_pricetag_dicts(img_dir):
@@ -46,41 +48,48 @@ def get_pricetag_dicts(img_dir):
     return dataset_dicts
 
 # Register test dataset
-DatasetCatalog.register("price_labels_valid", lambda: get_pricetag_dicts("/home/karun/detectron2/datasets/price_labels/price_labels_valid"))
-MetadataCatalog.get("price_labels_valid").set(thing_classes=["pricetag"])
+img_dir = "/home/karun95/master_thesis/datasets/russian_price_labels/price_labels_val/"  # Assuming the images are in the 'images' folder inside this directory
+DatasetCatalog.register("russian_price_labels_val", lambda: get_pricetag_dicts(img_dir))
+MetadataCatalog.get("russian_price_labels_val").set(thing_classes=["-", "name", "old_price", "price", "promotion"])
+
+
+# register_coco_instances("russian_price_labels_train", {},
+#                         "/home/karun95/master_thesis/datasets/russian_price_labels/price_labels_train/_annotations.coco.json",
+#                         "/home/karun95/master_thesis/datasets/russian_price_labels/price_labels_train/train")
+# register_coco_instances("russian_price_labels_val", {},
+#                         "/home/karun95/master_thesis/datasets/russian_price_labels/price_labels_val/_annotations.coco.json",
+#                         "/home/karun95/master_thesis/datasets/russian_price_labels/price_labels_val/valid")
 
 # Load the trained model configuration
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-# cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/retinanet_R_50_FPN_3x.yaml"))
-# cfg.MODEL.WEIGHTS = "./output/model_final.pth"  # Path to the trained model weights
-cfg.MODEL.WEIGHTS = "/home/karun/master_thesis/solo-learn/downstream/object_detection/fasterrcnn_resnet50.pth"  # Path to the trained model weights
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.90# Set a custom testing threshold
-cfg.MODEL.RETINANET.NMS_THRESH_TEST = 0.75
-cfg.DATASETS.TEST = ("pricetag_valid")
+cfg.merge_from_file("configs/faster_rcnn_R_50_C4_3x.yaml")
+cfg.MODEL.WEIGHTS = "/home/karun95/master_thesis/solo-learn/downstream/object_detection/output/model_final.pth"  # Path to the trained model weights
+cfg.DATALOADER.NUM_WORKERS = 4
+cfg.SOLVER.IMS_PER_BATCH = 1
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # Set a custom testing threshold
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
+cfg.DATASETS.TEST = ("russian_price_labels_val", )
 
 # Create a predictor
 predictor = DefaultPredictor(cfg)
-#
+
 # # Load test dataset
-dataset_dicts = get_pricetag_dicts("/home/karun/detectron2/datasets/price_labels/price_labels_valid")
+dataset_dicts = get_pricetag_dicts(img_dir)
 
 # Run inference and visualize results on a few test images
-for d in random.sample(dataset_dicts, 10):  # Visualize a few samples randomly
-    im = cv2.imread(d["file_name"])
-    outputs = predictor(im)
-    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get("pricetag_valid"), scale=0.9)
-    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    cv2.imshow('Predicted Image', v.get_image()[:, :, ::-1])
-    cv2.waitKey(0)
-cv2.destroyAllWindows()
+# for d in random.sample(dataset_dicts, 10):  # Visualize a few samples randomly
+#     im = cv2.imread(d["file_name"])
+#     outputs = predictor(im)
+#     v = Visualizer(im[:, :, ::-1], MetadataCatalog.get("russian_price_labels_val"), scale=0.9)
+#     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+#     cv2.imshow('Predicted Image', v.get_image()[:, :, ::-1])
+#     cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
 
 # Create evaluator
-evaluator = COCOEvaluator("price_labels_valid", cfg, False, output_dir="./output/")
-test_loader = build_detection_test_loader(cfg, "price_labels_valid")
+evaluator = COCOEvaluator("russian_price_labels_val", cfg, False, output_dir="./output/")
+test_loader = build_detection_test_loader(cfg, "russian_price_labels_val")
 
 # Run evaluation
 print(inference_on_dataset(predictor.model, test_loader, evaluator))
